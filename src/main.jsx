a02128ad5d4e862c72028f5058f318227f304679
Jocelito Oliveira Costa Junior <jocelito64@hotmail.com>
2024-02-09 12:04:14 -0300

    :construction:

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './pages/styles/global';
 
-import theme from './pages/styles/theme';
+import { AuthProvider } from './hooks/auth'
 
 import { Routes } from './routes';
+
+import theme from './pages/styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider  theme={theme}>
       <GlobalStyles />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
